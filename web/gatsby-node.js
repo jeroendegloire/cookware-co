8d26d622a2a01d9706f3798580c2bfc48691bbed
developerjahid <jhpabel@gmail.com>
2021-06-24 03:17:23 +0600

    all components done

--- web/gatsby-node.js
+++ web/gatsby-node.js
@@ -1,10 +1,12 @@
 const path = require('path')
 
 //create office page
-exports.createPages = async ({ graphql, actions }) => {
-  const tempate = path.resolve('./src/templates/office.js')
+exports.createPages = ({ graphql, actions }) => {
+  const tempateOffice = path.resolve('./src/templates/office.js')
+  const templateFactory = path.resolve('./src/templates/factory.js')
+  const templateBrand = path.resolve('./src/templates/brand.js')
 
-  const { data } = await graphql(`
+  const office = graphql(`
     {
       pages: allSanityOffice {
         nodes {
@@ -16,20 +18,96 @@ exports.createPages = async ({ graphql, actions }) => {
         }
       }
     }
-  `)
+  `).then(result => {
+    result.data.pages.nodes.forEach(page => {
+      console.info(`Creating page for: "${page.officeTitle}"...`)
 
-  data.pages.nodes.forEach(page => {
-    console.info(`Creating page for: "${page.officeTitle}"...`)
+      const slug = page.slug.current
 
-    const slug = page.slug.current
-
-    actions.createPage({
-      path: slug,
-      component: tempate,
-      context: {
-        id: page._id,
-        slug,
-      },
+      actions.createPage({
+        path: slug,
+        component: tempateOffice,
+        context: {
+          id: page._id,
+          slug,
+        },
+      })
     })
   })
+
+  const brand = graphql(`
+    {
+      pages: allSanityBrand {
+        edges {
+          node {
+            slug {
+              current
+            }
+            _id
+            brandName
+          }
+          next {
+            slug {
+              current
+            }
+            brandName
+          }
+          previous {
+            slug {
+              current
+            }
+            brandName
+          }
+        }
+      }
+    }
+  `).then(result => {
+    result.data.pages.edges.forEach(({ node, next, previous }) => {
+      console.info(`Creating page for: "${node.brandName}"...`)
+
+      const slug = node.slug.current
+
+      actions.createPage({
+        path: slug,
+        component: templateBrand,
+        context: {
+          id: node._id,
+          slug,
+          previous,
+          next,
+        },
+      })
+    })
+  })
+
+  const factory = graphql(`
+    {
+      pages: allSanityFactory {
+        nodes {
+          slug {
+            current
+          }
+          _id
+          factoryTitle
+        }
+      }
+    }
+  `).then(result => {
+    result.data.pages.nodes.forEach(page => {
+      console.info(`Creating page for: "${page.factoryTitle}"...`)
+
+      const slug = page.slug.current
+
+      actions.createPage({
+        path: slug,
+        component: templateFactory,
+        context: {
+          id: page._id,
+          slug,
+        },
+      })
+    })
+  })
+
+  return Promise.all([office, brand, factory])
 }
