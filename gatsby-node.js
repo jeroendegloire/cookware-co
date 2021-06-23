66b95180bb8f569f57d17c719691bb50c336ef44
developerjahid <jhpabel@gmail.com>
2021-06-24 01:42:12 +0600

    our factories done

--- gatsby-node.js
+++ gatsby-node.js
@@ -33,3 +33,37 @@ exports.createPages = async ({ graphql, actions }) => {
     })
   })
 }
+
+//create factory page
+exports.createPages = async ({ graphql, actions }) => {
+  const tempate = path.resolve('./src/templates/factory.js')
+
+  const { data } = await graphql(`
+    {
+      pages: allSanityFactory {
+        nodes {
+          slug {
+            current
+          }
+          _id
+          factoryTitle
+        }
+      }
+    }
+  `)
+
+  data.pages.nodes.forEach(page => {
+    console.info(`Creating page for: "${page.factoryTitle}"...`)
+
+    const slug = page.slug.current
+
+    actions.createPage({
+      path: slug,
+      component: tempate,
+      context: {
+        id: page._id,
+        slug,
+      },
+    })
+  })
+}
