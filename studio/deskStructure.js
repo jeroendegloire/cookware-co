4996d4feb801ae0323c9cf6cdbd675727359ece4
Jeroen Degloire <jeroen.degloire@hotmail.be>
2021-06-24 08:33:22 +0200

    Config preview env

--- studio/deskStructure.js
+++ studio/deskStructure.js
@@ -1,4 +1,5 @@
 import S from '@sanity/desk-tool/structure-builder'
+import resolveProductionUrl from './resolveProductionUrl'
 import SocialPreview from 'part:social-preview/component'
 import SeoPane from 'sanity-plugin-seo-pane'
 import Iframe from 'sanity-plugin-iframe-pane'
@@ -23,10 +24,7 @@ export default () =>
             .views([S.view.form(), S.view.component(SocialPreview()).title('Social'), S.view
             .component(Iframe)
             .options({
-              // Accepts an async function
               url: (doc) => resolveProductionUrl(doc),
-              // OR a string
-              url: `http://localhost:8000`,
             })
             .title('Preview'), 
         ])
